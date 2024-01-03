7913bd26fcd4169ca4f23908cd273047d5f34688
Dhruv Thakkar <dhruvthakkar2511@gmail.com>
2024-01-03 16:31:39 +0530

    tours

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import '../styles/tour-details.css'
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
+import {useParams} from 'react-router-dom'
+import tourData from '../assets/data/tours'
 
 const TourDetails = () => {
-  return (
-    <div>TourDetails</div>
-  )
+  const {id} = useParams()
+  const tour = tourData.find(tour=>tour.id === id)
+  const {photo, title, desc, price, reviews, city, distance, maxGroupSize} = tour
+
+  return <>
+  <section>
+    <Container>
+      <Col lg='8'>
+        <div className="tour__content">
+          <img src={photo} alt=""></img>
+          <div className="tour__info">
+            <h2>{title}</h2>
+          </div>
+        </div>
+      </Col>
+    </Container>
+  </section>
+  </>
 }
 
 export default TourDetails
