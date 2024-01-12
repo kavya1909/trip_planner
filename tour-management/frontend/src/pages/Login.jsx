a80548f7216c2895e8e1c1b90992dfe1e206ff71
Kavya <kavyapatel047@gmail.com>
2024-01-12 08:52:09 +0530

    reg

--- tour-management/frontend/src/pages/Login.jsx
+++ tour-management/frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import '../styles/login.css'
@@ -6,24 +6,39 @@ import loginImg from '../assets/images/login.png'
 import userIcon from '../assets/images/user.png'
 
 const Login = () => {
+  const [credentials,setCredentials]= useState({
+    email:undefined,
+    password:undefined
+  })
+  const handleChange=e=>{
+    setCredentials(prev => ({...prev, [e.target.id]:e.target.value}));
+  };
+  const handleClick=e=>{
+    e.preventDefault();
+  }
   return <section>
     <Container>
       <Row>
         <Col lg='8' className='m-auto'>
           <div className="login-container d-flex justify-content-between">
             <div className='login__img'>
-              <img src={loginImg} alt></img>
+              <img src={loginImg} alt></  img>
             </div>
             <div className='login__form'>
               <div className='user'>
                 <img src={userIcon} alt =""/>
               </div>
               <h2>Login</h2>
-              <Form>
+              <Form onSubmit={handleClick}>
                 <FormGroup>
-                  {/* <input type="text"|/> */}
+                 <input type="email" placeholder='Email' required id="email" onChange={handleChange}/>
                 </FormGroup>
+                <FormGroup>
+                  <input type="password" placeholder='Password' required id="password" onChange={handleChange}/>
+                </FormGroup>
+                <Button className="btn secondary__btn auth__btn" type="submit">Login</Button>
               </Form>
+              <p>Don't have an account?<Link to='/register'>Create</Link></p>
             </div>
           </div>
         </Col>
